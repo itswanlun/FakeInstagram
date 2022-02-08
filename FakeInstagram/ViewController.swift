b95bffaa13b275e9233d1bd6cdd892b131868b4f
wanlunzheng <wanlunzheng@gmail.com>
2022-02-08 15:32:41 +0800

    Initial Commit

---
+++ FakeInstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FakeInstagram
+//
+//  Created by Wan-lun Zheng on 2022/2/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
